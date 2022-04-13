8497c1249eba4c1b84a3701d15221bb7f3029796
Victor Ojog <infonetroom@yandex.ru>
2022-04-13 03:48:08 +0300

    modification input to button

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let nameProfile = document.querySelector('.profile__title');
 let jobProfile = document.querySelector('.profile__job');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
-let saveButton = document.querySelector('.popup__button');
+let saveButton = document.querySelector('.popup__container');
 
 function formSubmit(e) {
   e.preventDefault();
@@ -24,7 +24,7 @@ function closePopup() {
   openPopup.classList.remove('popup_opened');
 }
 
-saveButton.addEventListener('click', formSubmit);
+saveButton.addEventListener('submit', formSubmit);
 editPopup.addEventListener('click', showPopup);
 closeForm.addEventListener('click', closePopup);
 
