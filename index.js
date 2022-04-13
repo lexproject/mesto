943b0937e3d0a43c7b24158bcaad45e66fe3e345
Victor Ojog <infonetroom@yandex.ru>
2022-04-13 03:03:42 +0300

    review 1 proect mesto

---
+++ index.js
@@ -0,0 +1,30 @@
+let editPopup = document.querySelector('.profile__edit-button');
+let openPopup = document.querySelector('.popup');
+let closeForm = document.querySelector('.popup__close');
+let nameProfile = document.querySelector('.profile__title');
+let jobProfile = document.querySelector('.profile__job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let saveButton = document.querySelector('.popup__button');
+
+function formSubmit(e) {
+  e.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  closePopup();
+}
+
+function showPopup() {
+  openPopup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
+
+function closePopup() {
+  openPopup.classList.remove('popup_opened');
+}
+
+saveButton.addEventListener('click', formSubmit);
+editPopup.addEventListener('click', showPopup);
+closeForm.addEventListener('click', closePopup);
+
